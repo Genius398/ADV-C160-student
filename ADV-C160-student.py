6dc5026b92d8756e8830161d28e8f42abe2d17f3
Anantjit <anantjitchander@gmail.com>
2021-11-25 17:37:49 +0530

    Add files via upload

--- ADV-C160-student.py
+++ ADV-C160-student.py
@@ -24,6 +24,26 @@
  my_text = Text(root, height = 35, width = 80)
  my_text.place(relx = 0.5, rely=0.55, anchor = CENTER)
  
+ name=""
+ 
+ def openFile():
+     global name
+     my_text.delete(1.0, END)
+     input_file_name.delete(0, END)
+     text_file = filedialog.askopenfilename(title="Open Text file", filetypes=(("Text Files", "*.txt"),))
+     print(text_file)
+     name=os.path.basename(text_file)
+     formated_name = name.split('.')[0]
+     input_file_name.insert(END, formated_name)
+     root.title(formated_name)
+     text_file = open(name, 'r')
+     
+     paragraph = text_file.read()
+     my_text.insert(END, paragraph)
+     text_file.close()
+     
+
+    
  open_button = Button(root, image=open_img, text="openFile", command=openFile)
  open_button.place(relx = 0.05, rely=0.03, anchor=CENTER)
  
